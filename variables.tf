2552d0066af271c3e587b62a2351211d001458ca
cloudhashicorp <51735965+cloudhashicorp@users.noreply.github.com>
2019-06-12 21:12:42 +1000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,17 +6,17 @@ variable "region" {
 
 variable "access_key_id" {
 
-    default = "AKIATMN3XV2CK4MWJZFP"
+    default = "AKIATMN3XV2CK4MWJZFP" # You need to provide your own access key from AWS.
   
 }
 
 variable "secret_access_key_id" {
 
-    default = "rJ7J0MOAEUTIxqYuI9CFIHJ39bqYb57/aA858EuP"
+    default = "rJ7J0MOAEUTIxqYuI9CFIHJ39bqYb57/aA858EuP" # You need to provide your own secret key from AWS.
   
 }
 
-
+# These are only temporary information from Cloud Playground of Linux Academy
 # URL : https://232858824324.signin.aws.amazon.com/console?region=us-east-1
 # Username : cloud_user
 # Password: #5Sjjkmzoz
